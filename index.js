39806fe6f086541df7277218201ebf187470d669
Phuwit <phuwitput@gmail.com>
2021-07-02 20:36:34 +0700

    Added more sound and help command w/ help from Tah

--- index.js
+++ index.js
@@ -4,14 +4,19 @@ const fs = require('fs');
 
 client.once('ready', () => {
     console.log('Ready!');
+    client.user.setPresence({
+        activity:{
+            name: 'use !help'
+        }
+    })
 });
 
 // @everyone via 'activate'
 client.on('message', message => {
-    // if (message.content === '!help') {
-    //     message.channel.send('')
-    // }
-    if (message.content === '!activate') {
+    if (message.content === '!help') {
+        message.channel.send("```!activate & !deactvate\n!spdrn\n!B\n!fast\n!what\n!amogus\n!sesame\n!shesaid\n!rickroll\n!nyan\n!shootingstars\n!coffin\n!crazyfrog\n!curb\n!numa\n!fbi\n!hallelujah\n!dejavu\n!ppap\n!spiderman\n!pizza\n!lion```")
+    }
+    else if (message.content === '!activate') {
         const interval = setInterval(function() {
             message.channel.send('@everyone')
                 .catch(err => {
@@ -53,6 +58,54 @@ client.on('message', async message => {
     else if (message.content === '!sesame' && message.member.voice.channel) {
         playAudio('sesame.mp3', 1);
     }
+    else if (message.content === '!rickroll' && message.member.voice.channel) {
+        playAudio('rickroll.mp3', 1);
+    }
+    else if (message.content === '!shesaid' && message.member.voice.channel) {
+        playAudio('shesaid.mp3', 1);
+    }
+    else if (message.content === '!nyan' && message.member.voice.channel) {
+        playAudio('nyan.mp3', 1);
+    }
+    else if (message.content === '!shesaid' && message.member.voice.channel) {
+        playAudio('shesaid.mp3', 1);
+    }
+    else if (message.content === '!shootingstars' && message.member.voice.channel) {
+        playAudio('shootingstars.mp3', 0.8);
+    }
+    else if (message.content === '!coffin' && message.member.voice.channel) {
+        playAudio('coffin.mp3', 1);
+    }
+    else if (message.content === '!crazyfrog' && message.member.voice.channel) {
+        playAudio('crazyfrog.mp3', 1);
+    }
+    else if (message.content === '!curb' && message.member.voice.channel) {
+        playAudio('curb.mp3', 1);
+    }
+    else if (message.content === '!numa' && message.member.voice.channel) {
+        playAudio('numa.mp3', 1);
+    }
+    else if (message.content === '!fbi' && message.member.voice.channel) {
+        playAudio('fbi.mp3', 1);
+    }
+    else if (message.content === '!hallelujah' && message.member.voice.channel) {
+        playAudio('hallelujah.mp3', 1);
+    }
+    else if (message.content === '!dejavu' && message.member.voice.channel) {
+        playAudio('dejavu.mp3', 1);
+    }
+    else if (message.content === '!ppap' && message.member.voice.channel) {
+        playAudio('ppap.mp3', 1);
+    }
+    else if (message.content === '!spiderman' && message.member.voice.channel) {
+        playAudio('spiderman.mp3', 1);
+    }
+    else if (message.content === '!pizza' && message.member.voice.channel) {
+        playAudio('pizza.mp3', 1);
+    }
+    else if (message.content === '!lion' && message.member.voice.channel) {
+        playAudio('lion.mp3', 1);
+    }
 });
 
-client.login('ODU0MjkzMDE0NjY3MzI5NTM3.YMh0Xg.6QLWiXqDVTCOwGoTxmq5XrIaauI');
+client.login('discordBotToken');
