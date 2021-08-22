21f8503d56606c9b3ceee151075f2bc3a24528f9
Phuwit <phuwitput@gmail.com>
2021-08-22 23:09:53 +0700

    added coach

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 
+let coach_messages = ["เราผ่านหนึ่งปียังครับ", "กลุ่มวันอังคารยังไม่ถึงเวลาเลยครับ", "ปฏิบัติตามระเบียบจบครับ", "กลุ่มวันอังคารยังไม่ถึงเวลาเลยครับ", "ทหารรับทราบและรับปฏิบัติ", "รอให้ถึงเวลาที่เราเรียนก่อนครับ", "วันอังคารเจอกันครับ", "ไม่อยากพิมพ์ทางออนไลน์เลยครับ", "ได้รับฟังพร้อมกันเข้าใจพร้อมกันขับเคลื่อนไปทั้งชั้นปี 2 ทุกกลุ่มวันไม่ถามตอบทีละบุคคล", "https://cdn.discordapp.com/attachments/877743617991651348/879033527595831296/unknown.png", "นศท.นายใดไม่พร้อมที่จะศึกษาให้แจ้งรายชื่อครับ", "มีการเรียนการสอนผ่านระบบออนไลน์และให้ปฏิบัติตามระบบ ระเบียบ และวินัย เท่านั้น", "เอาตามนี้นักเรียน ถ้าไม่พร้อมแจ้งรายชื่อมาครับ", "เจอกันผ่านระบบออนไลน์วันอังคารครับ", "คุยกันวันอังคารครับ", ""]
+
+client.login(process.env.BOT_TOKEN);
+
 client.once('ready', () => {
     console.log('Ready!');
     client.user.setPresence({
@@ -14,7 +18,7 @@ client.once('ready', () => {
 // @everyone via 'activate'
 client.on('message', message => {
     if (message.content === '!help') {
-        message.channel.send("good bot v.3u1 https://github.com/phuwit/good-bot\n```!activate & !deactivate\n!spdrn\n!B\n!fast\n!what\n!amogus\n!sesame\n!shesaid\n!rickroll\n!nyan\n!shootingstars\n!coffin\n!crazyfrog\n!curb\n!numa\n!fbi\n!hallelujah\n!dejavu\n!ppap\n!spiderman\n!pizza\n!lion```")
+        message.channel.send("good bot v.3u2 https://github.com/phuwit/good-bot\n```!activate & !deactivate\n!spdrn\n!B\n!fast\n!what\n!amogus\n!sesame\n!shesaid\n!rickroll\n!nyan\n!shootingstars\n!coffin\n!crazyfrog\n!curb\n!numa\n!fbi\n!hallelujah\n!dejavu\n!ppap\n!spiderman\n!pizza\n!lion\n!cheng\n!kingdom```")
     }
     else if (message.content === '!activate') {
         const interval = setInterval(function() {
@@ -29,6 +33,10 @@ client.on('message', message => {
     }
     else if (message.content === '!deactivate'){
         clearInterval(task);
+    }
+    else if (message.content.startsWith("ครูครับ")){
+        let coach_response = coach_messages[Math.floor(Math.random() * wallpapers.length)];
+        message.reply(coach_response);
     }
 });
 
@@ -106,6 +114,11 @@ client.on('message', async message => {
     else if (message.content === '!lion' && message.member.voice.channel) {
         playAudio('lion.mp3', 1);
     }
+    else if (message.content === '!cheng' && message.member.voice.channel) {
+        playAudio('cheng.mp3', 1);
+    }
+    else if (message.content === '!kingdom' && message.member.voice.channel) {
+        playAudio('kingdom.mp3', 1);
+    }
 });
 
-client.login('bottoken');
